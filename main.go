f4891034f9e576ad384ac9f7c0f31ec0dcd9e1dd
Seth Hoenig <shoenig@duck.com>
2022-10-01 10:27:48 -0500

    cleanup: use logger and strings builder (#28)

--- main.go
+++ main.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/shoenig/ssh-key-sync/internal/command"
 	"github.com/shoenig/ssh-key-sync/internal/config"
+	"github.com/shoenig/ssh-key-sync/internal/logs"
 	"github.com/shoenig/ssh-key-sync/internal/netapi"
 	"github.com/shoenig/ssh-key-sync/internal/ssh"
 )
 
 func main() {
+	logger := logs.New(true)
 	args := config.ParseArguments(os.Args[0], os.Args[1:])
 	if args.GitHubUser == "" {
-		_, _ = fmt.Fprintf(os.Stderr, "ssh-key-sync requires --github-user\n")
-		os.Exit(1)
+		logger.Fatal("ssh-key-sync requires --github-user")
 	}
 
 	reader := ssh.NewKeysReader()
 	githubClient := netapi.NewGithubClient(args)
-
 	exec := command.NewExec(args.Verbose, reader, githubClient)
 	if err := exec.Execute(args); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "ssh-key-sync failed with error: %s\n", err)
-		os.Exit(1)
+		logger.Fatalf("ssh-key-sync failed with error: %s", err)
 	}
 }
